bee8bedcbc9ea1cc133f9727374ec68a9d1f53a9
Vinicius Souza <viniciuspodi@gmail.com>
2020-11-19 21:11:16 -0400

    Adicionando imagem na empresa

--- models/Empresa.js
+++ models/Empresa.js
@@ -1,113 +1,115 @@
-"use strict";
+'use strict'
 
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require('mongoose')
 
-class Empresa extends Schema{
-    constructor(){
-        super({
-            nome:{
-                type:String,
-                required:true,
-                unique:true
-            },
-            dataCriacao:{
-                type: Date,
-                default: new Date
-            },
-            cnpj:{
-                type:String,
-                required:true
-            },
-            email:{
-                type:String,
-                required:true
-            },
-            status:{
-                type:Boolean,
-                default:true,
-            },
-            tel:{
-                type:Number,
-                required:true
-            },
-            endereco:{
-                rua:{
-                    type:String,
-                    required:true
-                },
-                numero:{
-                    type:Number,
-                    required:true
-                },
-                bairro:{
-                    type:String,
-                    required:true
-                }
-            },
-            produtos:[
-                {
-                    nome:{
-                        type:String,
-                        required:true
-                    },
-                    descricao:{
-                        type:String,
-                        required:true
-                    },
-                    valor:{
-                        type:Number,
-                        required:true
-                    },
-                    status:{
-                        type:Boolean,
-                        default:true
-                    },
-                }
-            ],
-            transacoes:[
-                {
-                    empresaNome:{
-                        type:String
-                    },
-                    ativo:{
-                        type:Boolean,
-                        default:true
-                    },
-                    dataTransacoes:{
-                        type: String,
-                        default: Date.now
-                    },
-                    valor:{
-                        type:Number,
-                        required:true
-                    },
-                    status:{
-                        type:String,
-                        required:true
-                    },
-                    produtos:[
-                        {
-                            nome:{
-                                type:String,
-                                required:true
-                            },
-                            valor:{
-                                type:Number,
-                                required:true
-                            }
-                        }
-                    ],                    
-                    cliente: {
-                        type: Schema.Types.ObjectId,
-                        ref: "Cliente",
-                      },                   
-
-                }
-            ]
-        })
-        //Momento do registro do Schema do Mongoose
-        model("Empresas", this)
-    }
+class Empresa extends Schema {
+  constructor() {
+    super({
+      nome: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      dataCriacao: {
+        type: Date,
+        default: new Date()
+      },
+      cnpj: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      status: {
+        type: Boolean,
+        default: true
+      },
+      tel: {
+        type: Number,
+        required: true
+      },
+      imagem: {
+        type: String
+      },
+      endereco: {
+        rua: {
+          type: String,
+          required: true
+        },
+        numero: {
+          type: Number,
+          required: true
+        },
+        bairro: {
+          type: String,
+          required: true
+        }
+      },
+      produtos: [
+        {
+          nome: {
+            type: String,
+            required: true
+          },
+          descricao: {
+            type: String,
+            required: true
+          },
+          valor: {
+            type: Number,
+            required: true
+          },
+          status: {
+            type: Boolean,
+            default: true
+          }
+        }
+      ],
+      transacoes: [
+        {
+          empresaNome: {
+            type: String
+          },
+          ativo: {
+            type: Boolean,
+            default: true
+          },
+          dataTransacoes: {
+            type: String,
+            default: Date.now
+          },
+          valor: {
+            type: Number,
+            required: true
+          },
+          status: {
+            type: String,
+            required: true
+          },
+          produtos: [
+            {
+              nome: {
+                type: String,
+                required: true
+              },
+              valor: {
+                type: Number,
+                required: true
+              }
+            }
+          ],
+          cliente: {
+            type: Schema.Types.ObjectId,
+            ref: 'Cliente'
+          }
+        }
+      ]
+    })
+    //Momento do registro do Schema do Mongoose
+    model('Empresas', this)
+  }
 }
 
-module.exports = Empresa;
+module.exports = Empresa
